6f1fd707d029f5de5ab4aedfd4de8b3770061577
Dongheon Byeon <byeondongheon@gmail.com>
2026-02-28 21:41:01 +0900

    Fix: add missing quotes in school variable

--- day01_print.py
+++ day01_print.py
@@ -4,13 +4,12 @@ print("hello Python")
 
 
 age = 17
-school = CS high school
+school = "CS high school"
 print(f"나는 {school}에 다니는 {age}살 변동헌이다.")
 
 
-a=17
-b=18
+a = 17
+b = 18
 print(a+b)
 print(a*b)
 print(a-b)
-print(a**b)
